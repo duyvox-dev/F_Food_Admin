86f0682e61e0ef8fc82ac3844f39a954a7e79b15
duyvox-dev <vobaoduy123@gmail.com>
2022-10-25 21:18:34 +0700

    hotfix login service

--- src/services/authService.js
+++ src/services/authService.js
@@ -2,6 +2,6 @@ import { httpService } from './httpService';
 import { localStorageService } from './localService';
 export const authService = {
 	login: (token) => {
-		return httpService.post(`/customer/login`, token);
+		return httpService.post(`/customer/login`, { idToken: token });
 	},
 };
