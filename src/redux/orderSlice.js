a2fa53f4c7a95daa7e55b62dfbcdaa1b0c8f7bb5
duyvox-dev <vobaoduy123@gmail.com>
2022-11-13 20:33:07 +0700

    add order status filter

--- src/redux/orderSlice.js
+++ src/redux/orderSlice.js
@@ -2,81 +2,84 @@ import { createAsyncThunk, createSlice, createAction } from '@reduxjs/toolkit';
 import { message } from 'antd';
 import { orderService } from '../services/orderService';
 
-
 export const getOrderList = createAsyncThunk('orderSlice/getOrderList', async (pagination, thunkAPI) => {
-    try {
-        const result = await orderService.getOrderList(pagination);
-        return result.data;
-    } catch (error) {
-        message.error(error.response.data.message);
-        return thunkAPI.rejectWithValue();
-    }
+	try {
+		const result = await orderService.getOrderList(pagination);
+		let sortedOrder = result.data.results;
+		sortedOrder.sort(function (a, b) {
+			// Turn your strings into dates, and then subtract them
+			// to get a value that is either negative, positive, or zero.
+			return new Date(b.checkInDate) - new Date(a.checkInDate);
+		});
+		return sortedOrder;
+	} catch (error) {
+		message.error(error.response.data.message);
+		return thunkAPI.rejectWithValue();
+	}
 });
 
 export const getOrderInfo = createAsyncThunk('orderSlice/getOrderInfo', async (id, thunkAPI) => {
-    try {
-        const result = await orderService.getOrderInfo(id);
-        //thunkAPI.dispatch(toggleViewOrderDetail());
-        return result.data;
-    } catch (error) {
-        return thunkAPI.rejectWithValue();
-    }
+	try {
+		const result = await orderService.getOrderInfo(id);
+		//thunkAPI.dispatch(toggleViewOrderDetail());
+		return result.data;
+	} catch (error) {
+		return thunkAPI.rejectWithValue();
+	}
 });
 
 export const updateStatusOrder = createAsyncThunk('orderSlice/updateStatusOrder', async (data, thunkAPI) => {
-    console.log("data api; ", data)
-    try {
-        const result = await orderService.updateOrderStatus(data);
+	try {
+		const result = await orderService.updateOrderStatus(data);
 
-        thunkAPI.dispatch(toggleEditOrderModal());
-        thunkAPI.dispatch(getOrderList({ page: 1, pageSize: 10 }));
-        message.success('Cập nhật trạng thái thành công!');
+		thunkAPI.dispatch(toggleEditOrderModal());
+		thunkAPI.dispatch(getOrderList({ page: 1, pageSize: 10 }));
+		message.success('Cập nhật trạng thái thành công!');
 
-        return result.data;
-    } catch (error) {
-        message.error(error.response.data.message);
-        return thunkAPI.rejectWithValue();
-    }
+		return result.data;
+	} catch (error) {
+		message.error(error.response.data.message);
+		return thunkAPI.rejectWithValue();
+	}
 });
 
-
 const orderSlice = createSlice({
-    name: 'orderSlice',
-    initialState: {
-        orderList: [],
-        currentOrder: {},
-        modalEditOrder: false,
-        modalAdd: false,
+	name: 'orderSlice',
+	initialState: {
+		orderList: [],
+		currentOrder: {},
+		modalEditOrder: false,
+		modalAdd: false,
 
-        modalViewOrderDetail: false,
-    },
-    reducers: {
-        toggleEditOrderModal: (state, action) => {
-            state.modalEditOrder = !state.modalEditOrder;
-        },
-        toggleAddOrderModal: (state, action) => {
-            state.modalAdd = !state.modalAdd;
-        },
-        toggleViewOrderDetail: (state, action) => {
-            state.modalViewOrderDetail = !state.modalViewOrderDetail;
-        }
-    },
-    extraReducers: {
-        [getOrderList.pending]: (state, action) => {
-            state.orderList = [];
-        },
-        [getOrderList.fulfilled]: (state, action) => {
-            state.orderList = action.payload;
-        },
-        [getOrderList.rejected]: (state, action) => { },
-        [getOrderInfo.pending]: (state, action) => {
-            state.currentOrder = action.payload;
-        },
-        [getOrderInfo.fulfilled]: (state, action) => {
-            state.currentOrder = action.payload;
-        },
-        [getOrderInfo.rejected]: (state, action) => { },
-    },
+		modalViewOrderDetail: false,
+	},
+	reducers: {
+		toggleEditOrderModal: (state, action) => {
+			state.modalEditOrder = !state.modalEditOrder;
+		},
+		toggleAddOrderModal: (state, action) => {
+			state.modalAdd = !state.modalAdd;
+		},
+		toggleViewOrderDetail: (state, action) => {
+			state.modalViewOrderDetail = !state.modalViewOrderDetail;
+		},
+	},
+	extraReducers: {
+		[getOrderList.pending]: (state, action) => {
+			state.orderList = [];
+		},
+		[getOrderList.fulfilled]: (state, action) => {
+			state.orderList = action.payload;
+		},
+		[getOrderList.rejected]: (state, action) => {},
+		[getOrderInfo.pending]: (state, action) => {
+			state.currentOrder = action.payload;
+		},
+		[getOrderInfo.fulfilled]: (state, action) => {
+			state.currentOrder = action.payload;
+		},
+		[getOrderInfo.rejected]: (state, action) => {},
+	},
 });
 const { reducer, actions } = orderSlice;
 export const { toggleEditOrderModal, toggleAddOrderModal, toggleViewOrderDetail } = actions;
